6030314a56b2f33c46f75007290475d1e5742ddf
Greg Willson <greg@biomassiv.es>
2022-01-11 13:23:38 -0700

    first

---
+++ data/slideshow.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    image: () => require('~/assets/images/slideshow/image-1.jpg'),
+    color: 'light',
+    align: 'left',
+    title: '<b>Something</b> unforgettable',
+    description: 'have the time of your life'
+  },
+  {
+    image: () => require('~/assets/images/slideshow/image-2.jpg'),
+    color: 'light',
+    align: 'right',
+    title: '<b>Professional Móvil DJ service</b>',
+    description: 'Servicio completo y pro'
+  },
+  {
+    image: () => require('~/assets/images/slideshow/image-3.jpg'),
+    color: 'light',
+    align: 'center',
+    title: 'DJ <b>Ferchi</b> entertains',
+    description: 'Enjoy life'
+  },
+  {
+    image: () => require('~/assets/images/slideshow/image-4.jpg'),
+    color: 'light',
+    align: 'right',
+    title: '<b>Central & Northern NY</b>',
+    description: 'Wedding & event entertainment'
+  }
+]
---
+++ siteConfig.js
@@ -0,0 +1,15 @@
+export default {
+  site: {
+    name: 'DJ Ferchi New York Weddings & Events DJ',
+    slug: 'djferchi',
+    lang: 'en',
+    locale: 'en_US',
+    name_html: 'DJ <strong>Ferchi</strong>',
+    slogan: '',
+    author: 'DJ Ferchi Latin DJ New York',
+    description:
+      'Specialized music dance party sets for the following genres: Hip Hop, Reggaeton, Salsa, Bachata, Merengue, Cumbia and Crossover. For an awesome Latin/ hip-hop entertainment experience, 315-566-9138 or email me at pantojafernando@gmail.com .',
+    url: 'https://djferchi.com/',
+    copyright: 'Copyright © 2022 DJ Ferchi'
+  }
+}
